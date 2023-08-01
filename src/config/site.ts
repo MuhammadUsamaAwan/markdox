1d182965a58038522bbaa056498b7e39fd2a4b9b
Muhammad Usama <underusama@gmail.com>
2023-08-01 20:32:12 +0500

    font setup

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: 'MarkDox',
+  description:
+    'Create Stunning Documentation, Guides, and Blogs with MDXDocs! Unlock the Power of MDX Files for Effortless Content Creation.',
+};
