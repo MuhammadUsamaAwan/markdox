bc12cd43cde70d02aa79c81113c41311270b02dd
Muhammad Usama <underusama@gmail.com>
2023-08-02 00:09:41 +0500

    docs and guides

---
+++ src/app/guides/layout.tsx
@@ -0,0 +1,23 @@
+import { siteConfig } from '@/config/site';
+import { MainNav } from '@/components/layouts/main-nav';
+import { SiteFooter } from '@/components/layouts/site-footer';
+
+interface GuidesLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function GuidesLayout({ children }: GuidesLayoutProps) {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <header className='sticky top-0 z-40 w-full border-b bg-background'>
+        <div className='container flex h-16'>
+          <MainNav items={siteConfig.nav} />
+        </div>
+      </header>
+      <div className='container flex-1'>
+        <div className='mx-auto max-w-5xl'>{children}</div>
+      </div>
+      <SiteFooter />
+    </div>
+  );
+}
